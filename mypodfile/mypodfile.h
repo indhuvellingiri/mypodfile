49e6dcff489bfa4576c914e901bc2481405805fa
Indhumathy Vellingiri <indhumathy@ndot.in>
2020-02-19 14:55:33 +0530

    Initial project setup

---
+++ mypodfile/mypodfile.h
@@ -0,0 +1,19 @@
+//
+//  mypodfile.h
+//  mypodfile
+//
+//  Created by developer on 19/02/20.
+//  Copyright © 2020 ndot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mypodfile.
+FOUNDATION_EXPORT double mypodfileVersionNumber;
+
+//! Project version string for mypodfile.
+FOUNDATION_EXPORT const unsigned char mypodfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mypodfile/PublicHeader.h>
+
+
