1781566d5958568b4965ff6ed54ebe26a36e9dae
Indhumathy Vellingiri <indhumathy@ndot.in>
2020-02-19 15:50:17 +0530

    Added test cases

---
+++ mypodfile/mypodfile.swift
@@ -0,0 +1,25 @@
+//
+//  mypodfile.swift
+//  mypodfile
+//
+//  Created by developer on 19/02/20.
+//  Copyright © 2020 ndot. All rights reserved.
+//
+
+import Foundation
+
+
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
